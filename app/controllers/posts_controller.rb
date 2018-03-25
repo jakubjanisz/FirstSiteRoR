586afd4b80a44b104f2be283470cd20524eb1017
jakubjanisz <34276702+jakubjanisz@users.noreply.github.com>
2018-03-25 23:12:46 +0200

    Add files via upload

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,75 @@
+class PostsController < ApplicationController
+  layout :layout_or_not
+  before_action :authenticate, only: [:destroy, :confirm_destroy]
+
+  def new
+    @post = Post.new(author: session[:author])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      session[:author] = @post.author
+      flash[:notice] = "Post dodany pomyślnie."
+      redirect_to posts_path
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post zaktualizowany pomyślnie."
+      redirect_to posts_path
+    else
+      render action: 'edit'
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def published
+    @posts = Post.where(published: true)
+    render action: "index"
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def confirm_destroy
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "Pomyślnie usunięto wpis"
+    redirect_to posts_path
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :author, :body, :published)
+  end
+
+  def layout_or_not
+    if params[:skip_layout] == "true"
+      false
+    else
+      'application'
+    end
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic "Podaj haslo" do |name, password|
+      name == "username" && password == "secret"
+    end
+  end
+end
